3362f51d4a0fd97e3bc2b61ca8e5c3ae9d3f2c7e
jmmiller427 <jmmi234@g.uky.edu>
2017-11-05 13:21:39 -0500

    Initial Commit

---
+++ Papa Johns Voice Assistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Papa Johns Voice Assistant
+//
+//  Created by Matt Miller on 11/5/17.
+//  Copyright © 2017 CS499. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Papa Johns Voice Assistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Papa Johns Voice Assistant
+//
+//  Created by Matt Miller on 11/5/17.
+//  Copyright © 2017 CS499. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
