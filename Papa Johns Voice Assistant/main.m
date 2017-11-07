3d69a7032aa284af195b6012336a7ea2690c027c
Harrison Wainwright <Harrison@Jacks-MacBook-Pro.local>
2017-11-07 17:40:12 -0500

    harrison change

--- Papa Johns Voice Assistant/main.m
+++ Papa Johns Voice Assistant/main.m
@@ -1,8 +1,6 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-// comment harrison
-
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
