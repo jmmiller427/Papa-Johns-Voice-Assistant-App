3d63feb8b62962027de4c8998eb7cdefa723410e
jmmiller427 <jmmi234@g.uky.edu>
2017-11-07 20:05:20 -0500

    Added Podfile for ApiAI

---
+++ Pods/Target Support Files/Pods-Papa Johns Voice Assistant/Pods-Papa Johns Voice Assistant-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Papa_Johns_Voice_Assistant : NSObject
+@end
+@implementation PodsDummy_Pods_Papa_Johns_Voice_Assistant
+@end
---
+++ Pods/Target Support Files/Pods-Papa Johns Voice AssistantUITests/Pods-Papa Johns Voice AssistantUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Papa_Johns_Voice_AssistantUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Papa_Johns_Voice_AssistantUITests
+@end
