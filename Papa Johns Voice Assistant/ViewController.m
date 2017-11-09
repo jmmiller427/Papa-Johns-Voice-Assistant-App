43ad3ba7367a18f727b62071a8211e5632d79a79
jmmiller427 <jmmi234@g.uky.edu>
2017-11-08 20:42:41 -0500

    Speech to text prints out what you say

--- Papa Johns Voice Assistant/ViewController.m
+++ Papa Johns Voice Assistant/ViewController.m
@@ -47,47 +47,60 @@
 
 - (void)listen{
     
+    NSLog(@"Started Listen");
     // Create AVAudioEngine, SFSpeechAudioBufferRecognitionRequest,
     avAudio = [[AVAudioEngine alloc] init];
     bufferRecognitionRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
     
+    NSLog(@"Checking recognition task");
     // Check if the SFSpeechRecognitionTask is running or not, if it is
     // then stop it and set it to null
     if (recognitionTask){
         
+        NSLog(@"There was a recognition task, kill it");
         [recognitionTask cancel];
         recognitionTask = nil;
     }
     
+    NSLog(@"Create audio session");
     NSError *error;
     AVAudioSession *session = [AVAudioSession sharedInstance];
     [session setCategory:AVAudioSessionCategoryRecord error:&error];
     [session setActive:YES error:&error];
     
+    NSLog(@"Create input type");
     AVAudioInputNode *input = avAudio.inputNode;
     bufferRecognitionRequest.shouldReportPartialResults = YES;
     
+    NSLog(@"recognition task should create result");
     recognitionTask = [recognizer recognitionTaskWithRequest:bufferRecognitionRequest resultHandler:^(SFSpeechRecognitionResult * _Nullable result, NSError * _Nullable error) {
         BOOL isFinal = NO;
+        
+        NSLog(@"Before if (result)");
         if (result) {
             // Whatever you say in the mic after pressing the button should be being logged
             // in the console.
+            NSLog(@"Result should be printed");
             NSLog(@"RESULT:%@",result.bestTranscription.formattedString);
             isFinal = !result.isFinal;
         }
-        if (error) {
-            [avAudio stop];
-            [input removeTapOnBus:0];
-            bufferRecognitionRequest = nil;
-            recognitionTask = nil;
-        }
+        //NSLog(@"If (error)");
+        //if (error) {
+         //   NSLog(@"There was an error");
+           // [avAudio stop];
+          //  [input removeTapOnBus:0];
+          //  bufferRecognitionRequest = nil;
+          //  recognitionTask = nil;
+   //     }
     }];
     
+    NSLog(@"Create recording format");
     AVAudioFormat *recordingFormat = [input outputFormatForBus:0];
     [input installTapOnBus:0 bufferSize:1024 format:recordingFormat block:^(AVAudioPCMBuffer * _Nonnull buffer, AVAudioTime * _Nonnull when) {
         [bufferRecognitionRequest appendAudioPCMBuffer:buffer];
     }];
     
+    NSLog(@"Start the av audio engine");
     // Starts the audio engine, i.e. it starts listening.
     [avAudio prepare];
     [avAudio startAndReturnError:&error];
